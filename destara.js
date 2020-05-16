566e9e910622f5ded98ffe6ca2a077d27094f80f
papernas <code.arifin@gmail.com>
2020-05-17 00:40:35 +0700

    Create destara.js

---
+++ destara.js
@@ -0,0 +1,3 @@
+var alamat1 = Array("https://www.destara.xyz/p/the-development-of-music-and-current.html","https://www.destara.xyz/p/health-benefits-of-listening-to.html","https://www.destara.xyz/2019/09/apa-pilihan-kamu-dari-5-aplikasi-mp3.html","https://www.destara.xyz/2019/07/10-aplikasi-asuransi-mobil-terbaik-dan.html");
+var alamat = alamat1[Math.floor(Math.random()*alamat1.length)];
+window.location.href=alamat;
